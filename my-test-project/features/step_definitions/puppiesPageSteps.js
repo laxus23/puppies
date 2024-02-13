f48aa6ff4293e194fa2a98ac719a0d8ce592afec
laxus <laxus2323@o2.pl>
2024-02-13 13:04:31 +0100

    add generating reports

--- my-test-project/features/step_definitions/puppiesPageSteps.js
+++ my-test-project/features/step_definitions/puppiesPageSteps.js
@@ -4,6 +4,26 @@ const assert = require('assert').strict;
 
 let driver;
 setDefaultTimeout(20 * 1000); 
+const reporter = require('cucumber-html-reporter');
+
+const options = {
+    theme: 'bootstrap',
+    jsonFile: './reports/cucumber_report.json',
+    output: './reports/cucumber_report.html',
+    reportSuiteAsScenarios: true,
+    scenarioTimestamp: true,
+    launchReport: true,
+    metadata: {
+        "App Version": "1.0.0",
+        "Test Environment": "STAGING",
+        "Browser": "Chrome  97.0.4692.99",
+        "Platform": "Windows 10",
+        "Parallel": "Scenarios",
+        "Executed": "Local"
+    }
+};
+
+reporter.generate(options);
 
 // Selectors definition
 const selectors = {
